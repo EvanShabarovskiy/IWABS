0fd50eb0386b1e08c032536583ced008fdd01ad0
Dm Mov <movchanyukd@gmail.com>
2019-05-10 14:49:33 +0300

    add menu to app

--- Frontend/src/components/App.tsx
+++ Frontend/src/components/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Menu from './Menu/Menu';
 
 const App = () => (
   <Router>
     <>
+      <Menu />
       <Switch>
         <Route exact path="/" component={() => <h1>Welcome</h1>} />
       </Switch>
