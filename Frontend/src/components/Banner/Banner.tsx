c6121c73335f89486dd2bc34d8f4699fa23c0175
Dm Mov <movchanyukd@gmail.com>
2019-05-10 23:32:48 +0300

    banner v1.0

--- Frontend/src/components/Banner/Banner.tsx
+++ Frontend/src/components/Banner/Banner.tsx
@@ -2,7 +2,18 @@ import * as React from 'react';
 
 const Banner = () => (
   <div className="banner">
-    <h1 className="title">врятуй життя</h1>
+    <div className="motivation">
+      <h2 className="title">бути донором – це благородно і корисно!</h2>
+      <span className="motivation-text">
+        У організмі людини міститься в середньому близько 5 літрів крові, тобто
+        7% від ваги його тіла. Доза кроводачі абсолютно нешкідлива і безпечна
+        для здоров’я донора, є 450 мл.
+      </span>
+      <button className="btn feedback-btn">напишіть нам</button>
+    </div>
+    <div className="slogan-wrap">
+      <h1 className="title">врятуй чиєсь життя</h1>
+    </div>
   </div>
 );
 
