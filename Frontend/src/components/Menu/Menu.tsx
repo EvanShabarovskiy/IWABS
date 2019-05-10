48c53d27839c1bb6d0b7a03cddbdef3a425f7027
Dm Mov <movchanyukd@gmail.com>
2019-05-10 20:51:57 +0300

    add links

--- Frontend/src/components/Menu/Menu.tsx
+++ Frontend/src/components/Menu/Menu.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
+import Link from './Link';
 
 const Menu = () => {
   return (
-    <div>
-      <h1>menu works</h1>
-    </div>
+    <nav id="main-menu" className="menu">
+      <ul id="main-menu-links-wrap">
+        <li className="link-wrap">
+          <Link active to="/" label="головна" />
+        </li>
+        <li className="link-wrap">
+          <Link active to="/documents" label="документи" />
+        </li>
+        <li className="link-wrap">
+          <Link to="/for-donors" label="для донорів" />
+        </li>
+        <li className="link-wrap">
+          <Link to="/medicine" label="ліки" />
+        </li>
+        <li className="link-wrap">
+          <Link to="/news" label="новини" />
+        </li>
+        <li className="link-wrap">
+          <Link to="/photos" label="фотогалерея" />
+        </li>
+
+      </ul>
+    </nav>
   );
 };
 
