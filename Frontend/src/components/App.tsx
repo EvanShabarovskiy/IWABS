77dc99fc0004db0dad86daaf0de2830a6f8c808c
Dm Mov <movchanyukd@gmail.com>
2019-05-10 22:14:17 +0300

    add banner component

--- Frontend/src/components/App.tsx
+++ Frontend/src/components/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Menu from './Menu/Menu';
+import Banner from './Banner/Banner';
 
 const App = () => (
   <Router>
     <>
       <Menu />
+      <Banner />
       <Switch>
         <Route exact path="/" component={() => <h1></h1>} />
       </Switch>
---
+++ Frontend/src/components/Banner/Banner.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Banner = () => (
+  <div className="banner">
+    <h1 className="title">врятуй життя</h1>
+  </div>
+);
+
+export default Banner;
