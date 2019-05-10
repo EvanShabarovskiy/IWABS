ac97b348e717a3d90f4d34d6c7dfcf0bd7290e85
Evan Shabarovskiy <ivanshabarovskij@gmail.com>
2019-05-10 13:26:17 +0300

    DB settings is done

---
+++ Backend/Backend/Models/IWABS_Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class IWABS_Context : DbContext
+    {
+        public IWABS_Context(DbContextOptions<IWABS_Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> News { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Admin>().HasData(
+                new Admin[]
+                {
+                    new Admin { Id = Guid.NewGuid().ToString(), Login = "iwabs@gmail.com", Password = "admin" }
+                }
+            );
+        }
+    }
+}
---
+++ Backend/Backend/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string PublishingDate { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+    }
+
+}
