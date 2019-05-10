af5da7c0ef7a7a96e4b77ac5074a8ab2e2d79486
Dm Mov <movchanyukd@gmail.com>
2019-05-10 13:18:17 +0300

    add default route and routing is done

--- Frontend/src/components/App.tsx
+++ Frontend/src/components/App.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <Router>
     <>
+      <Switch>
+        <Route exact path="/" component={() => <h1>Welcome</h1>} />
+      </Switch>
     </>
   </Router>
 );
