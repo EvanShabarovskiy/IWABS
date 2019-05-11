5013929cd412daa4fca1d98fb5ea2dd7a3f13796
Dm Mov <movchanyukd@gmail.com>
2019-05-11 11:15:59 +0300

    add history item component and props

---
+++ Frontend/src/components/History/HistoryItem.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { StatelessComponent } from 'react';
+import { IHistoryItemProps } from '../../interfaces/IHistoryItemProps';
+
+export const HistoryItem: StatelessComponent<IHistoryItemProps> = ({ title, text }) => (
+  <div className="history-item">
+    <h4 className="history-item-title">{title}</h4>
+    <p className="history-item-text">{text}</p>
+  </div>
+);
