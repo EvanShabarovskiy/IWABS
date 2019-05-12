4fdd00544344b83898d81f754ca08e4af8d99479
Dm Mov <movchanyukd@gmail.com>
2019-05-12 15:37:38 +0300

    add circle adn circles components

---
+++ Frontend/src/components/Circles/Circle.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Circle = () => (
+    <div className="circle">
+      <i className="fa fa-user"></i>
+    </div>
+  );
+
+export default Circle;
---
+++ Frontend/src/components/Circles/Circles.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Circle from './Circle';
+
+const Circles = () => (
+  <div className="circles">
+    <Circle />
+    <Circle />
+    <Circle />
+  </div>
+);
+
+export default Circles;
