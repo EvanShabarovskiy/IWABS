ccb904a0d56bcc04e48baa5d308eace118cdd906
Evan Shabarovskiy <ivanshabarovskij@gmail.com>
2019-05-13 12:05:02 +0300

    Create AuthService, AuthController and BuildToken method

---
+++ Backend/Backend/Services/AuthService.cs
@@ -0,0 +1,43 @@
+﻿using Backend.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Services
+{
+    public class AuthService
+    {
+        IConfiguration configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string BuildToken(Admin admin)
+        {
+            Claim[] claims = new[] {
+                new Claim(JwtRegisteredClaimNames.NameId, admin.Id),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]));
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                configuration["JWT:Issuer"],
+                configuration["JWT:Issuer"],
+                claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
