d38172c94a25ea23bbc33042932fa5e86e8ca2a9
Dm Mov <movchanyukd@gmail.com>
2019-05-14 12:56:10 +0300

    Change export of admin container component

--- Frontend/src/components/App.tsx
+++ Frontend/src/components/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Menu } from './Menu/Menu';
 import Footer from './Footer/Footer';
 import { routes_data } from '../assets/constants/routes_data';
-import AdminContainer from './routes/Admin/AdminContainer';
+import { AdminContainer } from './routes/Admin/AdminContainer';
 
 const App = () => (
   <Router>
--- Frontend/src/components/routes/Admin/AdminContainer.tsx
+++ Frontend/src/components/routes/Admin/AdminContainer.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import Admin from './Admin';
-import { withRouter } from 'react-router';
 import { StatelessComponent } from 'react';
 import SignInContainer from './SignIn/SignInContainer';
 
-const AdminContainer: StatelessComponent<{}> = props => {
+export const AdminContainer: StatelessComponent<{}> = props => {
   console.log(props);
   return <SignInContainer />;
 };
-
-export default withRouter(AdminContainer);
