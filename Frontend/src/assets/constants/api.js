ae0da602509ca9597625cbcbc36a0418ae12f320
Dm Mov <movchanyukd@gmail.com>
2019-05-18 17:17:50 +0300

    remove bin

--- Frontend/src/assets/constants/api.js
+++ Frontend/src/assets/constants/api.js
@@ -1,1 +1,3 @@
-export const api = 'http://localhost:58054/api/';
+export const host = 'http://localhost:58054/';
+export const api = host + 'api/';
+export const stat = host + 'Static/';
--- Frontend/src/components/News/Post.jsx
+++ Frontend/src/components/News/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { stat } from '../../assets/constants/api';
 
 const Post = ({ post: { title, publishingDate, image, text } }) => (
   <div className="post">
     <h2 className="post-title">{title}</h2>
     <p className="publishing-date">дата публікації: <span className="number bold">{publishingDate}</span></p>
-    <img className="img post-img" src={image} />
+    <img className="img post-img" src={stat + image} />
     <p className="post-text">{text}</p>
     <button className="learn-more-btn">Більше</button>   
   </div>
