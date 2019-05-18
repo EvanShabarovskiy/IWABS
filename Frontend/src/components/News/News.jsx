1c342e7b5a9c02ecfae91a290892540f0b712550
Dm Mov <movchanyukd@gmail.com>
2019-05-18 19:50:33 +0300

    Style news

--- Frontend/src/components/News/News.jsx
+++ Frontend/src/components/News/News.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Post from './Post';
 
 const News = ({ news }) => (
-  <div className="news">
+  <section className="news section">
+    <h1 className="section-title">новини</h1>
     {news.map(post => (
       <Post key={post.id} post={post} />
     ))}
-  </div>
+  </section>
 );
 
 export default News;
--- Frontend/src/components/News/Post.jsx
+++ Frontend/src/components/News/Post.jsx
@@ -4,10 +4,10 @@ import { stat } from '../../assets/constants/api';
 const Post = ({ post: { title, publishingDate, image, text } }) => (
   <div className="post">
     <h2 className="post-title">{title}</h2>
-    <p className="publishing-date">дата публікації: <span className="number bold">{publishingDate}</span></p>
+    <p className="publishing-date">{publishingDate}</p>
     <img className="img post-img" src={stat + image} />
     <p className="post-text">{text}</p>
-    <button className="learn-more-btn">Більше</button>   
+    <button className="read-more btn">читати далі</button>   
   </div>
 );
 
