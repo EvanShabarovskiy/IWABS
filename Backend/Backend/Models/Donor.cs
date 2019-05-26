504ef32acd5e91167f8e1a7727609a0a56ddbee8
Evan Shabarovskiy <ivanshabarovskij@gmail.com>
2019-05-27 01:26:44 +0300

    Add DonorsController and was changed Donor model

--- Backend/Backend/Models/Donor.cs
+++ Backend/Backend/Models/Donor.cs
@@ -9,9 +9,11 @@ namespace Backend.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
+        public float BloodDonated { get; set; }
+        public string BloodGroup { get; set; }
         public string DateofBirth { get; set; }
         public string Adress { get; set; }
-        public string BloodGroup { get; set; }
        }
 
 }
