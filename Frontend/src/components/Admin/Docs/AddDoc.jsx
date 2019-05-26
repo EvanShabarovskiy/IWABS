500f947f755f5ecdb609153ac15d01fd485f2e7e
Dm Mov <movchanyukd@gmail.com>
2019-05-26 23:30:28 +0300

    create AddDoc layout

--- Frontend/src/components/Admin/Docs/AddDoc.jsx
+++ Frontend/src/components/Admin/Docs/AddDoc.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Field from '../../_General/Field';
+import { Upload, Button, Icon } from 'antd';
 
-const AddDoc = () => {
-  return (
-    <form className="form add-doc-form">
-      
-    </form>
-  );
-};
+const AddDoc = ({ uploadOptions, data: { name }, change, onSubmit }) => (
+  <form className="form add-doc-form" onSubmit={onSubmit}>
+    <Upload {...uploadOptions}>
+      <Button size="large" className="upload">
+        <Icon type="upload" /> Click to Upload
+      </Button>
+    </Upload>
+    <Field name="name" value={name} placeholder="назва документа" change={change} />
+    <button className="btn sub-btn">додати</button>
+  </form>
+);
 
 export default AddDoc;
--- Frontend/src/components/Admin/Docs/AddDocContainer.jsx
+++ Frontend/src/components/Admin/Docs/AddDocContainer.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddDoc from './AddDoc';
+import withForm from '../../withForm';
 
-const AddDocContainer = () => {
+const AddDocContainer = ({ setValue, data }) => {
+  const [file, setFile] = useState(null);
+  const uploadOptions = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      setAddPostData(data => ({
+        ...data,
+        file: info.file.originFileObj
+      }))
+      if (info.file.status !== 'uploading') {
+        console.log(info, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('submit');
+  }
   return (
-    <AddDoc />
+    <AddDoc uploadOptions={uploadOptions} change={setValue} data={data} onSubmit={onSubmit} />
   );
 };
 
-export default AddDocContainer;
+const initialState = { 
+  name: ''
+}
+
+export default withForm(initialState, initialState)(AddDocContainer);
