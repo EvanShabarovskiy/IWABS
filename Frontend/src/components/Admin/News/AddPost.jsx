54a145c65ddace49f6dbe1067430cc30a9d887d3
Dm Mov <movchanyukd@gmail.com>
2019-05-28 20:56:57 +0300

    my upload component is done

--- Frontend/src/components/Admin/News/AddPost.jsx
+++ Frontend/src/components/Admin/News/AddPost.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Upload, Button, Icon } from 'antd';
 import Field from '../../_General/Field';
+import Upload from '../../_General/Upload';
 
-const AddPost = ({ data: { title, text }, change, onSubmit, uploadOptions }) => (
+const AddPost = ({ data: { title, text }, change, onSubmit, uploadOptions}) => (
   <form className="form add-post-form" onSubmit={onSubmit}>
-    <Upload {...uploadOptions}>
-      <Button size="large" className="upload">
-        <Icon type="upload" /> натисніть для завантаження
-      </Button>
-    </Upload>
+    <Upload {...uploadOptions} label="натисніть для завантаження" />
     <Field name="title" value={title} placeholder="заголовок" change={change} />
     <Field isTextarea name="text" value={text} placeholder="текст" change={change} />
     <button type="submit" className="btn sub-btn">добавити</button>
--- Frontend/src/components/Admin/News/AddPostContainer.jsx
+++ Frontend/src/components/Admin/News/AddPostContainer.jsx
@@ -2,53 +2,46 @@ import React, { useState } from 'react';
 import AddPost from './AddPost';
 import Axios from 'axios';
 import { api } from '../../../assets/constants/api';
-import { message } from 'antd';
+import withForm from '../../withForm';
 
-const AddPostContainer = () => {
-  const [addPostData, setAddPostData] = useState({ title: '', text: '', file: null});
-  const uploadOptions = {
-    name: 'file',
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    headers: {
-      authorization: 'authorization-text',
-    },
-    onChange(info) {
-      setAddPostData(data => ({
-        ...data,
-        file: info.file.originFileObj
-      }))
-      if (info.file.status !== 'uploading') {
-        console.log(info, info.fileList);
-      }
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-  };
-  const change = ({ target: { value, name }}) => {
-    setAddPostData(data => ({
-      ...data,
-      [name]: value
-    }))
+const AddPostContainer = ({ data, setValue }) => {
+  const [uploadOptions, setUploadOptions] = useState({
+    file: null, 
+    fileName: '', 
+    disabled: false
+  });
+
+  const fileChange = ({ target: { files } }) => {
+    console.log('files', files);
+    setUploadOptions({
+      file: files[0],
+      fileName: files[0].name,
+      disabled: true
+    })
   }
   const onSubmit = e => {
     e.preventDefault();
     let formData = new FormData();
-    const { title, text, file } = addPostData;
+    const { title, text } = data;
+    const { file } = uploadOptions;
+
     formData.append("title", title);
     formData.append("text", text);
     formData.append("file", file);
     console.log(formData);
-    console.log(addPostData)
     Axios.post(api + 'news', formData)
       .then(({ data }) => console.log('success', data))
       .catch(error => console.log(error.response))
   }
+  uploadOptions['fileChange'] = fileChange;
   return (
-    <AddPost uploadOptions={uploadOptions} data={addPostData} change={change} onSubmit={onSubmit} />
+    <AddPost uploadOptions={uploadOptions} data={data} change={setValue} onSubmit={onSubmit} />
   );
 };
 
-export default AddPostContainer;
+const initialState = {
+  title: '',
+  text: ''
+}
+
+export default withForm(initialState, initialState)(AddPostContainer);
