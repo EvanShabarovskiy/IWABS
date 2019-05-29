03a979a9949bff2ae62c076556e48e6a4f3291db
Dm Mov <movchanyukd@gmail.com>
2019-05-29 23:20:57 +0300

    some fixing

--- Frontend/src/components/_Forms/AddMedicine.jsx
+++ Frontend/src/components/_Forms/AddMedicine.jsx
@@ -3,7 +3,7 @@ import Field from '../_General/Field';
 import Form from '../_General/Form';
 
 const AddMedicine = ({ data: { name, releaseForm, unit, amount }, change, onSubmit}) => (
-  <Form formClass="form add-medicine-form" onSubmit={onSubmit}  title="нові ліки" buttonText="додати ліки">
+  <Form formClass="add-medicine-form" onSubmit={onSubmit}  title="нові ліки" buttonText="додати ліки">
     <Field name="name" value={name} placeholder="назва" change={change} />
     <Field name="releaseForm" value={releaseForm} placeholder="форма випуску" change={change} />
     <Field name="unit" value={unit} placeholder="о. в." change={change} />
