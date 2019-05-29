38b14c693e3ffcfedcd23bca77ffd1cdf51ee89c
Dm Mov <movchanyukd@gmail.com>
2019-05-29 23:05:40 +0300

    use form component in medicine form

--- Frontend/src/components/_Forms/AddMedicine.jsx
+++ Frontend/src/components/_Forms/AddMedicine.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Field from '../_General/Field';
+import Form from '../_General/Form';
 
 const AddMedicine = ({ data: { name, releaseForm, unit, amount }, change, onSubmit}) => (
-  <form className="form add-medicine-form" onSubmit={onSubmit}>
+  <Form className="form add-medicine-form" onSubmit={onSubmit}  title="нові ліки" buttonText="додати ліки">
     <Field name="name" value={name} placeholder="назва" change={change} />
     <Field name="releaseForm" value={releaseForm} placeholder="форма випуску" change={change} />
     <Field name="unit" value={unit} placeholder="о. в." change={change} />
     <Field name="amount" value={amount} placeholder="кількість" change={change} />
-    <button className="btn sub-btn">додати</button>
-  </form>
+  </Form>
 );
 
 export default AddMedicine;
