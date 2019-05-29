1d503c68020a5a5171dd1be651357b53c0a36826
Dm Mov <movchanyukd@gmail.com>
2019-05-29 22:39:57 +0300

    create Form component

---
+++ Frontend/src/components/_General/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cx from 'classnames';
+
+const Form = ({ formClass, onSubmit, children, buttonText, title }) => (
+  <form 
+    className={cx('grid', 'form', !!formClass && formClass)} 
+    onSubmit={onSubmit}
+  >
+    <h1 className={cx('title', !!formClass && `${formClass}-title`)}>{title}</h1>
+    {children}
+    <button 
+      type="submit" 
+      className={cx('btn', 'sub-btn', !!formClass && `${formClass}-btn`)}
+    >
+      {buttonText}
+    </button>
+  </form>
+);
+
+export default Form;
