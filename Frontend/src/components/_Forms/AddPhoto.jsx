ae4a7f4452e55a652f728d76d6a8504be83391c0
Dm Mov <movchanyukd@gmail.com>
2019-05-29 23:07:16 +0300

    use form component in photo form

--- Frontend/src/components/_Forms/AddPhoto.jsx
+++ Frontend/src/components/_Forms/AddPhoto.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Field from '../_General/Field';
 import Upload from '../_General/Upload';
+import Form from '../_General/Form';
 
 const AddPhoto = ({ uploadOptions, data: { title }, change, onSubmit }) => (
-  <form className="form add-photo-form" onSubmit={onSubmit}>
+  <Form className="form add-photo-form" onSubmit={onSubmit} title="нове фото" buttonText="додати фото">
     <Upload {...uploadOptions} label="натисніть для завантаження" />
     <Field name="title" value={title} placeholder="заголовок зображення" change={change} />
-    <button className="btn sub-btn">додати</button>
-  </form>
+  </Form>
 );
 
 export default AddPhoto;
