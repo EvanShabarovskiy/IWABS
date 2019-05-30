b516fd20689b55220e4bf6043c447e6db4f4619a
Dm Mov <movchanyukd@gmail.com>
2019-05-30 17:56:09 +0300

    add redux-thunk and use it

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,11 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import { get as getCookies } from 'js-cookie';
 import { get } from 'axios';
 import { toggleSignedIn } from './actions';
 import { api } from '../assets/constants/api';
+import thunk from 'redux-thunk';
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const getSignedIn = () => {
   const token = getCookies('token');
