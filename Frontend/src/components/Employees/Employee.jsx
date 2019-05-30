810e2cd3fc6456e9820f93c63be20993bf0cf0c2
Dm Mov <movchanyukd@gmail.com>
2019-05-30 13:45:13 +0300

    employees work is done

--- Frontend/src/components/Employees/Employee.jsx
+++ Frontend/src/components/Employees/Employee.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Icon } from 'antd';
 
 export const Employee = ({ position, fullName, phone }) => (
   <div className="employee">
-    <h5 className="position">{position}</h5>
-    <p className="full-name">{fullName}</p>
+    <h3 className="full-name">{fullName}</h3>
+    <p className="position">{position}</p>
     <p className="phone">
-      тел. <span className="number">{phone}</span>{' '}
+      <Icon type="phone" className="phone-icon" />
+      <span className="number">{phone}</span>
     </p>
   </div>
 );
