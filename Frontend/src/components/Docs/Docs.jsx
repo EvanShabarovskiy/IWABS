bc5a93895f8d2b85a0e8e9f7925ba59601d7d6c1
Dm Mov <movchanyukd@gmail.com>
2019-06-01 19:48:10 +0300

    add page class to helpers

--- Frontend/src/components/Docs/Docs.jsx
+++ Frontend/src/components/Docs/Docs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DocList from './DocList';
 
 const Docs = ({ docs }) => (
-  <section className="section docs">
+  <section className="section page docs">
     <h1 className="section-title">документи</h1>
     <DocList docs={docs} />
   </section>
--- Frontend/src/components/News/News.jsx
+++ Frontend/src/components/News/News.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Post from './Post';
 
 const News = ({ news }) => (
-  <section className="news section">
+  <section className="news page section">
     <h1 className="section-title">новини</h1>
     {news.map(post => (
       <Post key={post.id} post={post} />
