b8225762fe9aa8536e50c6b352d55e9744f9a6e4
Dm Mov <movchanyukd@gmail.com>
2019-06-03 22:47:04 +0300

    how to becom a donor section is done

--- Frontend/src/components/ForDonors/HowToBecomeADonor.jsx
+++ Frontend/src/components/ForDonors/HowToBecomeADonor.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import info from '../../assets/data/howToBecomeADonor';
+import List from '../_General/List';
+
+const { basicRequirements, youCanBecomeADonorIf } = info;
 
 const HowToBecomeADonor = () =>
   <section className="sub-section how-to-become-a-donor">
     <h1 className="section-title">як стати донором</h1>
-    
+    <section className="content-section">
+      <h3 className="sub-title">що потрібно зробити</h3>
+      <List list={basicRequirements} />
+    </section>
+    <section className="content-section">
+      <p className="text">Ви можете стати донором, якщо:</p>
+      <List list={youCanBecomeADonorIf} />
+    </section>
   </section>
 
 export default HowToBecomeADonor;
