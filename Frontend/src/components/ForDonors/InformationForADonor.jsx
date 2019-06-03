cab22f816e9adf31a4519517f7ff17a72e3f65d0
Dm Mov <movchanyukd@gmail.com>
2019-06-03 22:40:03 +0300

    information for a donor section is done

--- Frontend/src/components/ForDonors/InformationForADonor.jsx
+++ Frontend/src/components/ForDonors/InformationForADonor.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import info from '../../assets/data/informationForADonor';
+import List from '../_General/List';
+
+const { whatADonorShouldKnow, recommendationsToDonors, intervalsBetweenDonationsOfBlood } = info;
 
 const InformationForADonor = () =>
   <section className="sub-section information-for-a-donor">
     <h1 className="section-title">інформація для донора</h1>
-    
+    <section className="content-section">
+      <h3 className="sub-title">що повинен знати донор</h3>
+      <p className="text">{whatADonorShouldKnow}</p>
+    </section>
+    <section className="content-section">
+      <h3 className="sub-title">рекомендації донорам</h3>
+      <List list={recommendationsToDonors} />
+    </section>
+    <section className="content-section">
+      <h3 className="sub-title">дотримуйте покладені інтервали між дацами крові та її компонентів</h3>
+      <p className="text">{intervalsBetweenDonationsOfBlood.preTitle}</p>
+      <List {...intervalsBetweenDonationsOfBlood} />
+    </section>
   </section>
 
 export default InformationForADonor;
--- Frontend/src/components/_General/List.jsx
+++ Frontend/src/components/_General/List.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const List = ({ list }) =>
   <ul className="list">
-    { list.map((item, i) => <li className="list-item">{item}</li>) }
+    { list.map((item, i) => <li key={i} className="list-item">{item}</li>) }
   </ul>
 
 export default List;
