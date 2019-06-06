f8e34c5ee6191acaa218ebe62bcc608b47bef7cc
Dm Mov <movchanyukd@gmail.com>
2019-06-06 14:44:49 +0300

    fix achevements

--- Frontend/src/components/Achievements/Achievement.jsx
+++ Frontend/src/components/Achievements/Achievement.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Circle = ({ iconClass, title, value }) => (
+const Achievement = ({ iconClass, title, value }) =>
   <div className="achievement">
     <div className="icon-wrap">
-      <i className={`${iconClass} icon`} />
+      <i className={`${iconClass} achievement-icon`} />
     </div>
     <h5 className="achievement-title">{title}</h5>
     <span className="achievement-value">{value}</span>
   </div>
-);
 
-export default Circle;
+export default Achievement;
--- Frontend/src/components/Achievements/Achievements.jsx
+++ Frontend/src/components/Achievements/Achievements.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Achievement from './Achievement';
 import achievements from '../../assets/data/achievements';
 
-const Achievements = () => (
+const Achievements = () =>
   <section className="section achievements">
     <h1 className="section-title">досягнення</h1>
     { achievements.map(achievement => <Achievement key={achievement.title} {...achievement} />) }
   </section>
-);
 
 export default Achievements;
