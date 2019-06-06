b49707c4c9775054e81d1bd79fd95ca4971ea809
Evan Shabarovskiy <ivanshabarovskij@gmail.com>
2019-06-06 17:11:04 +0300

    Gallery is done

--- Frontend/src/components/Gallery/Photo.jsx
+++ Frontend/src/components/Gallery/Photo.jsx
@@ -3,6 +3,9 @@ import { gallery } from '../../assets/constants/api';
 
 const Photo = ({ title, imageName }) => (
   <div className="photo">
+    <section className="image-overlay">
+      <p className="image-title">{title}</p>
+    </section>
     <img className="img" src={gallery + imageName} />
   </div>
 );
