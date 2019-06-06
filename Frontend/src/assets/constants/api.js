44c79161403943d07def6b384dcd9bfc7fc2d62e
Dm Mov <movchanyukd@gmail.com>
2019-06-06 14:13:12 +0300

    change api constant

--- Frontend/src/assets/constants/api.js
+++ Frontend/src/assets/constants/api.js
@@ -2,5 +2,6 @@ export const host = 'http://localhost:58054/';
 // export const host = 'http://192.168.0.116:5000/';
 
 export const api = host + 'api/';
-export const stat = host + 'Static/images/';
-export const gallery = host + 'static/images/gallery/';
+export const stat = host + 'static/';
+export const gallery = stat + 'images/gallery/';
+export const news = stat + 'images/news/'
--- Frontend/src/components/News/PostContainer.jsx
+++ Frontend/src/components/News/PostContainer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-import { stat } from '../../assets/constants/api';
+import { news } from '../../assets/constants/api';
 import { removePost } from '../../store/news/actions';
 import Post from './Post';
 
 const PostContainer = ({ post, removePost, signedIn }) => {
   post['showControls'] = signedIn;
   post['onRemove'] = () => confirm('Ви дійсно хочете видалити цей запис?') && removePost(post.id);
-  post['src'] = stat + 'images/news/' + post.image;
+  post['src'] = news + post.image;
   return <Post {...post} />
 };
 
